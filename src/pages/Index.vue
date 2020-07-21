6234a58f71881a4b4635a8fc0e49fb6512a8a210
mirko.pleli <mirko.pleli@outlook.de>
2020-07-21 15:13:08 +0200

    Add link to router, own vue page for scanner.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,30 +1,14 @@
 <template>
-  <q-page class="flex flex-center flex-direction_column">
-    <q-btn color="primary" @click="toggle()">QR-Code Scannen</q-btn>
-    <Scanner v-if="isOpen"></Scanner>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    >
   </q-page>
 </template>
 
 <script>
-import Scanner from 'components/Scanner'
-
 export default {
-  name: 'PageIndex',
-  components: { Scanner },
-  data () {
-    return {
-      isOpen: false
-    }
-  },
-  methods: {
-    toggle () {
-      this.isOpen = !this.isOpen
-    }
-  }
+  name: 'PageIndex'
 }
 </script>
-
-<style lang="sass" scoped>
-.flex-direction_column
-  flex-direction: column
-</style>
---
+++ src/pages/Scanner.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="flex flex-center flex-direction_column">
+    <q-btn color="primary" @click="toggle()">QR-Code Scannen</q-btn>
+    <Scanner v-if="isOpen"></Scanner>
+  </q-page>
+</template>
+
+<script>
+import Scanner from 'components/Scanner'
+
+export default {
+  name: 'Scanner',
+  components: { Scanner },
+  data () {
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+    toggle () {
+      this.isOpen = !this.isOpen
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.flex-direction_column
+  flex-direction: column
+</style>
