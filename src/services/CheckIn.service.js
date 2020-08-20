f12b11b7b82ae1ac8d46c1c2b2c30567d71be764
Stephan Strittmatter <stephan.strittmatter@sybit.de>
2020-08-20 10:23:19 +0200

    correction of Date String

--- src/services/CheckIn.service.js
+++ src/services/CheckIn.service.js
@@ -62,7 +62,7 @@ class CheckInService {
 
   getDateString (date) {
     const year = date.getFullYear()
-    const month = date.getMonth() < 10 ? '0' + date.getMonth() : date.getMonth()
+    const month = date.getMonth() < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1)
     const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
 
     return year + month + day
