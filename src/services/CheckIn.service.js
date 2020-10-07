f379398ab59439f4463581965f11a85a940c41e0
Christian Wolter <48556854+chrisWolter@users.noreply.github.com>
2020-10-07 20:31:21 +0200

    showBookedRoom: enhanced DateString Function

--- src/services/CheckIn.service.js
+++ src/services/CheckIn.service.js
@@ -62,10 +62,13 @@ class CheckInService {
 
   getDateString (date) {
     const year = date.getFullYear()
-    const month = date.getMonth() < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1)
-    const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
-
+    const month = padDate(date.getMonth())
+    const day = padDate(date.getDate())
     return year + month + day
+
+    function padDate (number) {
+      return number.toString().padStart(2, '0')
+    }
   }
 }
 
