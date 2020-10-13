9221bf86aa20845cc92a81c28941d94970bed971
Christian Wolter <48556854+chrisWolter@users.noreply.github.com>
2020-10-13 18:29:38 +0200

    sort-by-location: Add Fontawesome

--- src/main.js
+++ src/main.js
@@ -3,13 +3,29 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+
+//  Bootstrap & main Variables
 import './assets/app.sass'
+
+//  Firebase
 import firebase from 'firebase'
 import * as fbService from './services/firebase'
 
+//  Fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+//  Firebase
 Vue.prototype.$firebase = firebase
 
 fbService.fBInit()
+
+//  Fontawesome
+// For Documentation see here: https://github.com/FortAwesome/vue-fontawesome
+library.add(faUserSecret, faChevronLeft)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
