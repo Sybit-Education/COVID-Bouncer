1a401d92ca5e21ef91f39cd8f40389c66c3d605e
chrisWolter <wolter-christian@outlook.com>
2020-10-13 12:26:55 +0200

    vue-master: Add bootstrap

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import './assets/app.sass'
 
 Vue.config.productionTip = false
 
