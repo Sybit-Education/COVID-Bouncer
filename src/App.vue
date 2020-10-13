b97981b241ea418298bdb2ef73700a5eb6a526d5
Christian Wolter <48556854+chrisWolter@users.noreply.github.com>
2020-10-13 21:18:00 +0200

    sort-by-location: Fixed router error

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <div id="nav" class="row main-nav">
-      <div class="col-2 align-self-center text-center"><div v-if="isIndex"><router-link :to="-1" class="sy-color"><font-awesome-icon :icon="['fas', 'chevron-left']" /></router-link></div></div>
+      <div class="col-2 align-self-center text-center"><div v-if="isIndex"><div @click="$router.go(-1)" class="sy-color"><font-awesome-icon :icon="['fas', 'chevron-left']" /></div></div></div>
       <div class="col-8 align-self-center text-center"><h1 class="mt-0 mb-0 ">SyRooms</h1></div>
       <div class="col-2 align-self-center text-center"></div>
     </div>
+    <div class="container">
     <router-view/>
+    </div>
   </div>
 </template>
 
 <style lang="sass">
 .main-nav
-  margin-left: 12px
-  margin-right: 12px
   height: 60px
-  margin-bottom: 1rem
+  margin-bottom: 3rem
 </style>
 
 <script>
 export default {
   computed: {
     isIndex () {
-      return this.$route.name !== 'Home'
+      if (window.history.length > 2) {
+        return this.$route.name !== 'Home'
+      }
+      return false
     }
   }
 }
