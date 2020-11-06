6e899a36a8413e00c3b00c0c4742358ebd59097a
Christian Wolter <48556854+chrisWolter@users.noreply.github.com>
2020-11-06 23:04:22 +0100

    Add Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <div id="app" class="container">
+    <navbar></navbar>
     <router-view/>
     <notifications group="error" position="top center"/>
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/base/Navbar'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style lang="sass">
 .main-nav
