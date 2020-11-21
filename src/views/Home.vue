6d929de644637b7de4f47d50643d3d04e22c1b15
Christian Wolter <48556854+chrisWolter@users.noreply.github.com>
2020-11-21 15:55:17 +0100

    get RoomIDs of CollectioNGroupQuerys

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,39 @@
 
 <script>
 import LocationList from '@/components/LocationList'
+import { $db } from '@/services/firebase'
+// import { userService } from '@/services/UserService'
 
 export default {
   name: 'Home',
   components: {
     LocationList
+  },
+  data () {
+    return {
+      currentDate: new Date().toISOString().slice(0, 10)
+    }
+  },
+  async mounted () {
+    await this.getTodaysRoom()
+  },
+  methods: {
+    getTodaysRoom: async function () {
+      // const currentUserID = await userService.currentUser()
+      // const userRef = await $db().doc('user/' + currentUserID)
+      const querySnapshot = await $db().collectionGroup('CheckIn').where('date', '==', this.currentDate).get()
+      querySnapshot.forEach((doc) => {
+        console.log(doc.ref.parent.parent.id)
+        /* const user = doc.data().user
+         user.forEach(user => {
+          user.get().then(snap => {
+            console.log(snap.data())
+            console.log(user.getParent().getParent())
+          })
+        }) */
+        console.log(doc.id, ' => ', doc.data())
+      })
+    }
   }
 }
 </script>
