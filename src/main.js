90e8a8ea68b51ec23aa080811d0e37fb13b43148
mirko-pleli <mirko.pleli@outlook.de>
2020-12-04 17:00:49 +0100

    Delete unsued store.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import store from './store'
 import { BootstrapVue } from 'bootstrap-vue'
 //  Bootstrap & main Variables
 import './assets/app.sass'
@@ -36,6 +35,5 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
